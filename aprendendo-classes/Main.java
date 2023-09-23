a9cf8bce9b52b990028d4c109b025444a35d26fe
Matheus-Assis05 <matheusassisdev@gmail.com>
2023-09-23 17:27:18 -0300

    Inicio do Projeto

---
+++ aprendendo-classes/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+
+        // Nesse programa vou fazer um sistema básico de menu para aprender sobre
+        // classes e objetos em java
+        // Classe em opsMenu.java. Lang: PT-BR
+
+    }
+}
---
+++ aprendendo-classes/opsMenu.java
@@ -0,0 +1,7 @@
+public class opsMenu {
+    //Métodos da classe:
+    public void cmdHelp(){
+        System.out.println("Comandos: \n"+
+        "sobre - Descrição báscia do produto");        
+    }
+}
